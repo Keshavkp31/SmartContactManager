5c3a4866754603bd253182420c7a8397183540c8
Keshavkp31 <patil.keshav1229@gmail.com>
2024-12-20 19:43:59 +0530

    Database table and page maping are done

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,7 +3,9 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @Controller
@@ -31,5 +33,24 @@ public class PageController {
         System.out.println("services page loading");
         return "services";
     }
+    // contact 
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+    //login
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+    //signup
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
+    
+    
+
+    
 
 }
